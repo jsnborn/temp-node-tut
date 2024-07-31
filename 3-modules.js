6e6ca62e7736feee6530febc5588d1d1701ea615
Jason <jasonwong9572@gmail.com>
2024-07-30 23:25:31 -0700

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const chickenBake = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+
+console.log(chickenBake)
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
+
